9de36516485b64a6d3dba1e11aab12a034bb0c1a
Andrew Hoang <53106989+vorxcomp@users.noreply.github.com>
2020-09-04 01:39:36 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,28 @@
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+
+local UI = Material.Load({
+     Title = "SFOTHHaxx",
+     Style = 3,
+     SizeX = 250,
+     SizeY = 150,
+     Theme = "Dark"
+})
+
+local Page = UI.New({
+    Title = "Main"
+})
+
+Page.Button({
+    Text = "Infinite ForceField",
+    Callback = function()
+       workspace.username:WaitForChild("ShieldSphere"):Destroy()
+    end,
+})
+
+Page.Button({
+    Text = "Infinite Invisibility",
+    Callback = function()
+       workspace.username:WaitForChild("ShadowSphere"):Destroy()
+    end,
+})
+
